9b89523981a71faaa9a5f5fc4359a712f229cd4b
Ian Mugambi <ian.mugambi1@student.moringaschool.com>
2024-03-22 18:27:23 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Define companyName as a constant
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood and companyCeo
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+module.exports = companyName;
